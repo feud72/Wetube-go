58abfdc2b013a5f9718f82eee62fa76577e1e947
feud72 <feud72@gmail.com>
2020-01-27 15:38:11 +0900

    #2.6 Express Core: Middlewares

--- webserver.go
+++ webserver.go
@@ -3,9 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
+
+// 미들웨어를 정의하는 방법. 
+// 이 방식으로 initialization logic 을 넣을 수 있다.
+func DummyMiddleware() gin.HandlerFunc {
+	fmt.Println("I'm a dummy!")
+	return func(c *gin.Context){
+		c.Next()
+	}
+}
 
 func handleHome(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "Home"})
@@ -18,7 +28,11 @@ func handleProfile(c *gin.Context) {
 
 func main() {
 	addr := ":3000"
-	g := gin.Default()
+	g := gin.New()
+	g.Use(gin.Logger())
+	g.Use(gin.Recovery())
+	g.Use(DummyMiddleware())
+
 	g.GET("/", handleHome)
 	g.GET("/profile", handleProfile)
 	log.Fatal(http.ListenAndServe(addr, g))
